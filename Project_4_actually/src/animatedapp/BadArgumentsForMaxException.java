1d951d1621ff051bb13d75abf9b49b69280e136e
Kanon Klassen <ksklassen@cpp.edu>
2025-05-03 22:08:36 -0700

    Question 7

---
+++ Project_4_actually/src/animatedapp/BadArgumentsForMaxException.java
@@ -0,0 +1,18 @@
+package animatedapp;
+
+
+
+
+/**
+* Indicate that the arguments for max were bad. It is unchecked.
+* 
+* @author Charles Hoot
+* @version 5.0
+*/
+public class BadArgumentsForMaxException extends RuntimeException
+{
+	public BadArgumentsForMaxException(String reason)
+	{
+		super(reason);
+	}
+}
--- Project_4_actually/src/animatedapp/RevesActionThread.java
+++ Project_4_actually/src/animatedapp/RevesActionThread.java
@@ -1,5 +1,270 @@
 package animatedapp;
 
-public class RevesActionThread {
+import animatedapp.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+    
 
-}
+/**
+ *  A thread that is used to solve Reve's problem - It is animated.
+ * 
+ * @author Charles Hoot 
+ * @version 5.0
+ */
+
+    
+public class RevesActionThread extends ActionThread
+{
+    
+    /**
+     * Constructor for objects of class RevesActionThread
+     */
+    public RevesActionThread()
+    {
+        super();
+
+    }
+
+    public String getApplicationTitle()
+    {
+        return "Reve's Puzzle (Skeleton)";
+    }
+    
+    
+
+    // **************************************************************************
+    // This is application specific code
+    // **************************************************************************    
+
+    // These are the variables that are parameters of the application and can be
+    // set via the application specific GUI
+    // Make sure they are initialized
+    private int disksToUse = 10;
+   
+    
+    
+    private int disks;
+    
+    // Displayed objects
+    private Pole a, b, c, d;
+    private int movesMade;
+    private String moveString;
+    
+    public void init() 
+    {
+    	disks = disksToUse;  // Number of disks to use (taken from the GUI or default)
+        movesMade = 0;
+        moveString = "";
+
+        // Create the four poles
+        a = new Pole("Pole A", disks);  // Pole A will hold the disks initially
+        b = new Pole("Pole B", disks);  // Pole B is an extra pole
+        c = new Pole("Pole C", disks);  // Pole C is another extra pole
+        d = new Pole("Pole D", disks);  // Pole D is the destination pole
+
+        // Add disks to Pole A (largest to smallest)
+        for (int i = disks; i > 0; i--) {
+            a.addDisk(new Disk(i));  // Add a disk of size 'i' to Pole A
+        }
+
+        // INITIALIZATION CODE from Kanon
+
+    }
+        
+
+    public void executeApplication()
+    {
+    	moveDisk(a,b);
+    }
+
+    /**
+     * Move a disk from one pole to another pole.
+     *
+     * @param from      The source pole.
+     * @param to        The destination pole.
+     */
+    public void moveDisk(Pole from, Pole to)
+    {
+    	// Remove the top disk from the 'from' pole
+        Disk toMove = from.removeDisk();
+        
+        // If there is a disk to move (i.e., the pole isn't empty), add it to the 'to' pole
+        if (toMove != null) {
+            to.addDisk(toMove);  
+        }
+
+        movesMade++;
+        moveString = "Move #" + movesMade 
+                        + ": Moved disk " + toMove.getSize() 
+                        + " from " + from.getName() 
+                        + " to " + to.getName();
+                            
+        // Pause the animation after the move
+        animationPause();           
+    }
+
+    public void towersOfHanoi(int n, Pole from, Pole to, Pole extra)
+    {
+        if (n == 1) {
+            // Base case: Move the single disk directly from 'from' to 'to'
+            moveDisk(from, to);
+        } else {
+            // Recursive case:
+            
+            // Step 1: Move n-1 disks from 'from' to 'extra', using 'to' as an auxiliary
+            towersOfHanoi(n - 1, from, extra, to);
+            
+            // Step 2: Move the nth disk from 'from' to 'to'
+            moveDisk(from, to);
+            
+            // Step 3: Move the n-1 disks from 'extra' to 'to', using 'from' as an auxiliary
+            towersOfHanoi(n - 1, extra, to, from);
+        }
+    }
+    // ADD METHODS HERE
+    
+    /***************************************************************************
+     * *************************************************************************
+     * ALL THE CODE PAST THIS POINT SHOULD NOT BE CHANGED
+     * *************************************************************************
+     * *************************************************************************
+     */
+    
+    
+    
+    private static int DISPLAY_HEIGHT = 300;
+    private static int DISPLAY_WIDTH = 500;
+    
+    public JPanel createAnimationPanel()
+    {
+        return new AnimationPanel();
+    }
+    
+    private static int NORTH_PANEL_HEIGHT = 50;
+    private static int INDENT = 50;
+    private static int SCALE = 2;
+    private static int TEXT_HEIGHT = 30;
+    private static int MAX_DISKS = 15;
+    
+
+    // This privately defined class does the drawing the application needs
+    public class AnimationPanel extends JPanel
+    {
+        public AnimationPanel()
+        {
+            super();
+            setSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+        }
+        
+        public void paintComponent(Graphics g)
+        {
+            int sw;
+            super.paintComponent(g);
+            int delta = Disk.BASEWIDTH*SCALE*MAX_DISKS;
+            
+            // draw the move string if it has a value
+            if(moveString != null)
+            {
+                g.drawString(moveString, INDENT, NORTH_PANEL_HEIGHT + TEXT_HEIGHT);
+            }
+            
+            
+            // draw the four poles if they have been created
+            FontMetrics fm = g.getFontMetrics();            
+            if(d!= null)
+            {
+                a.drawOn(g, delta/2, DISPLAY_HEIGHT-2*TEXT_HEIGHT, SCALE);
+                sw = fm.stringWidth(a.getName());
+                g.drawString(a.getName(), delta/2 - sw/2, DISPLAY_HEIGHT-TEXT_HEIGHT);
+                
+                b.drawOn(g, 3*delta/2, DISPLAY_HEIGHT-2*TEXT_HEIGHT, SCALE);
+                sw = fm.stringWidth(b.getName());
+                g.drawString(b.getName(), 3*delta/2 - sw/2, DISPLAY_HEIGHT-TEXT_HEIGHT);
+                
+                c.drawOn(g, 5*delta/2, DISPLAY_HEIGHT-2*TEXT_HEIGHT, SCALE);
+                sw = fm.stringWidth(c.getName());
+                g.drawString(c.getName(), 5*delta/2 - sw/2, DISPLAY_HEIGHT-TEXT_HEIGHT);
+                
+                d.drawOn(g, 7*delta/2, DISPLAY_HEIGHT-2*TEXT_HEIGHT, SCALE);
+                sw = fm.stringWidth(d.getName());
+                g.drawString(d.getName(), 7*delta/2 - sw/2, DISPLAY_HEIGHT-TEXT_HEIGHT);
+            }
+
+        }
+    }
+    
+    // **************************************************************************
+    // This is the application specific GUI code
+    // **************************************************************************    
+
+    private JTextField disksTextField;
+    private JLabel setupStatusLabel;
+    private JPanel setupPanel;
+    
+    public void setUpApplicationSpecificControls()
+    {
+        getAnimationPanel().setLayout(new BorderLayout());
+        
+        
+        disksTextField = new JTextField("");
+        disksTextField.addActionListener(
+            new ActionListener() 
+            {
+                public void actionPerformed(ActionEvent event) 
+                {
+                    disksTextFieldHandler();
+                    getAnimationPanel().repaint();
+                }
+            }
+        );
+
+
+        
+        setupStatusLabel = new JLabel("");
+        
+        setupPanel = new JPanel();
+        setupPanel.setLayout(new GridLayout(2,2));
+        
+        setupPanel.add(new JLabel("Number of disks (1-15):"));
+        setupPanel.add(disksTextField);
+        setupPanel.add(setupStatusLabel);
+        
+        getAnimationPanel().add(setupPanel,BorderLayout.NORTH);
+               
+    }
+
+   
+   
+    private void disksTextFieldHandler()
+    {
+    try
+        {
+            if(applicationControlsAreActive())   // Only change if we are in the setup phase
+            {
+                String input = disksTextField.getText().trim();
+                int value = Integer.parseInt(input);
+                if( value>=1 &&value <= MAX_DISKS)
+                {
+                    disksToUse = value;
+                    setupStatusLabel.setText("Set number of disks to " + disksToUse);
+                }
+                else
+                {
+                    setupStatusLabel.setText("Bad value for number of disks");
+                }
+                init();
+                getAnimationPanel().repaint();
+                
+            }
+        }
+        catch(Exception e)
+        {
+            // don't change the delta if we had an exception
+            setupStatusLabel.setText("Need integer value for number of disks");
+        }
+    
+    }  
+            
+} // end class RevesActionThread
+
